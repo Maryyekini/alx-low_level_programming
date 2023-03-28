d103bd0caa567436a447605744fe4f32903b2975
Maryyekini <yekinimaryfm@gmail.com>
2023-03-27 17:58:51 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *puts2 - function should print only one instead of two
+  *Starting with the first one
+  *@str: input
+  *Return: print
+  */
+void puts2(char *str)
+{
+	int logi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o - 1; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			putchar(str[0]);
+		}
+	}
+	putchar('\n')
+}
