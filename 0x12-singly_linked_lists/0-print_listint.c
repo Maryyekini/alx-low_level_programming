beaff4484652c13ea9cb326a7a3a7d5d5e454035
Maryyekini <maryyekinifm@gmail.com>
2023-05-02 13:46:15 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_listint.c
+++ 0x12-singly_linked_lists/0-print_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all elements of a list
+ * print_listint - Prints all elements of a linked list
  * @h: Pointer to a list
  *
  * Return: Number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t j = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		++j;
+		++i;
 	}
-	return (j);
+	return(i);
 }
